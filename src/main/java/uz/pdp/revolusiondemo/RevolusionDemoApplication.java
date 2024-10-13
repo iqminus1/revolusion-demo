5f085371a87d20b672e40562bb9b656a281f560d
Abdulaziz <wwxeww1wwxeww@gmail.com>
2024-10-13 11:25:39 +0500

    created

---
+++ src/main/java/uz/pdp/revolusiondemo/RevolusionDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.revolusiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolusionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevolusionDemoApplication.class, args);
+    }
+
+}
